c98c1a9de80dc7c31174a6fc7ca97aeebae594d4
Rub27182n <if717710@iteso.mx>
2022-10-22 00:40:31 -0500

    switch to plotly for prop viz and variable deletion

--- functions.py
+++ functions.py
@@ -1,19 +1,34 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
-def subsampling(df,target_variable):
+
+def subsampling(df, target_variable):
     # Obtener el valor de la clase que tiene una menor proporción
     min_sample_size = df.groupby(target_variable).precio_unitario.count().min()
 
-    # Guardar en un diccionario las categorías de nuestra variable objetivo asi como el conteo de cada una 
-    balance_dict = df.groupby(target_variable).precio_unitario.count().to_dict()
+    # Guardar en un diccionario las categorías de nuestra variable objetivo asi como el conteo de cada una
+    balance_dict = df.groupby(
+        target_variable).precio_unitario.count().to_dict()
 
     # Realizar el balanceo (solo funciona para 2 categorías)
     for key in balance_dict:
         if balance_dict[key] != min_sample_size:
-            max_class_sample = df[df[target_variable] == key].sample(min_sample_size)
+            max_class_sample = df[df[target_variable]
+                                  == key].sample(min_sample_size)
             print(f'clase {key} balanceada')
         if balance_dict[key] == min_sample_size:
             min_class_sample = df[df[target_variable] == key]
 
-    return pd.concat([max_class_sample,min_class_sample])
-        
+    return pd.concat([max_class_sample, min_class_sample])
+
+
+def label_encoder(data: "DataFrame"):
+    for col in data.columns:
+        if data[col].dtype == "object":
+            le = LabelEncoder()
+            data[col].fillna("None", inplace=True)
+            le.fit(list(data[col].astype(str).values))
+            data[col] = le.transform(list(data[col].astype(str).values))
+        else:
+            data[col].fillna(-999, inplace=True)
+    return data
--- visualizations.py
+++ visualizations.py
@@ -1,58 +1,86 @@
 # Libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import plotly.graph_objects as go
 import seaborn as sns
+import plotly.express as px
 plt.style.use('ggplot')
 
 
-def balancing(df,target_variable='outlier'):
+def balancing(df, target_variable='outlier'):
     conteo = df.groupby(target_variable).precio_unitario.count()
-    plt.figure(figsize=(7,6))
-    plt.bar(df[target_variable].unique(),conteo.values)
+    plt.figure(figsize=(7, 6))
+    plt.bar(df[target_variable].unique(), conteo.values)
     plt.title('Balanceo de nuestra variable objetivo')
     plt.grid(alpha=.4)
     print(f'La clase de NO outliers tiene {conteo[0]}')
     print(f'La clase de Outliers tiene {conteo[1]}')
 
 
-def evaluation_metrics(history,n_epochs):
+def evaluation_metrics(history, n_epochs):
     plt.plot(
-    np.arange(1, n_epochs+1), 
-    history.history['loss'], label='Loss'
+        np.arange(1, n_epochs+1),
+        history.history['loss'], label='Loss'
     )
     plt.plot(
-        np.arange(1,  n_epochs+1), 
+        np.arange(1,  n_epochs+1),
         history.history['accuracy'], label='Accuracy'
     )
     plt.plot(
-        np.arange(1,  n_epochs+1), 
+        np.arange(1,  n_epochs+1),
         history.history['precision'], label='Precision'
     )
     plt.plot(
-        np.arange(1,  n_epochs+1), 
+        np.arange(1,  n_epochs+1),
         history.history['recall'], label='Recall'
     )
     plt.plot(
-        np.arange(1,  n_epochs+1), 
+        np.arange(1,  n_epochs+1),
         history.history['auc'], label='AUC'
     )
     plt.title('Evaluation metrics', size=20)
     plt.xlabel('Epoch', size=14)
-    plt.legend();
+    plt.legend()
 
 
-def plot_cm(labels, predictions,threshold):
-  cm = confusion_matrix(labels, predictions)
-  plt.figure(figsize=(9,7))
-  sns.heatmap(cm, annot=True, fmt="d")
-  plt.title('Confusion matrix @{:.2f}'.format(threshold))
-  plt.ylabel('Actual label')
-  plt.xlabel('Predicted label')
+def plot_cm(labels, predictions, threshold):
+    cm = confusion_matrix(labels, predictions)
+    plt.figure(figsize=(9, 7))
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.title('Confusion matrix @{:.2f}'.format(threshold))
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
 
-  print('Legitimate NO_Outliers Detected (True Negatives): ', cm[0][0])
-  print('Legitimate NO_Outliers Incorrectly Detected (False Positives): ', cm[0][1])
-  print('Outliers Missed (False Negatives): ', cm[1][0])
-  print('Outliers Detected (True Positives): ', cm[1][1])
-  print('Total Outliers: ', np.sum(cm[1]))
-  print('Total No_Outliers: ', np.sum(cm[0]))
+    print('Legitimate NO_Outliers Detected (True Negatives): ', cm[0][0])
+    print(
+        'Legitimate NO_Outliers Incorrectly Detected (False Positives): ', cm[0][1])
+    print('Outliers Missed (False Negatives): ', cm[1][0])
+    print('Outliers Detected (True Positives): ', cm[1][1])
+    print('Total Outliers: ', np.sum(cm[1]))
+    print('Total No_Outliers: ', np.sum(cm[0]))
+
+
+def balancing2(data, bar_chart=False):
+    class_count = pd.value_counts(data['outlier'], sort=True).sort_index()
+    fig = go.Figure(data=[go.Pie(labels=['non-outlier', 'outlier'],
+                                 values=class_count,
+                                 pull=[0, 0.1],
+                                 opacity=0.85)])
+    fig.update_layout(
+        title_text="Price Classification in Outlier - Non-Outlier")
+    fig.show()
+
+    if bar_chart:
+        fig = px.histogram(data, x="outlier",
+                           title='Price Classification in Outlier - Non-Outlier',
+                           opacity=0.85,  # represent bars with log scale
+                           color='outlier', text_auto=True)
+
+        fig.update_layout({
+            "plot_bgcolor": "rgba(0, 0, 0, 0)",
+            "paper_bgcolor": "rgba(0, 0, 0, 0)",
+        })
+
+        fig.show()
